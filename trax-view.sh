3f928851e3c5b635f4b977d0e5c46c890642043f
mjhd <mjhd@devlion.net>
2016-05-18 17:01:06 +0900

    change to use cmake in trax-viewer

--- trax-view.sh
+++ trax-view.sh
@@ -4,11 +4,13 @@ trax="./out/bin/trax"
 trax_viewer="./trax-viewer/out/bin/trax-viewer"
 
 if [ ! -e $trax ]; then
+    cmake .
     make
 fi
 
 if [ ! -e $trax_viewer ]; then
     cd ./trax-viewer/
+    cmake .
     make
     cd ../
 fi
